57f911c955b3ef7e6bf8675b67011e373993ee6c
Alex Groff <amgroff@ucsc.edu>
2023-05-01 14:32:32 -0700

    Final comments and citations

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
+/*
+    Name: Alex Groff
+    Mod Title: Rocket Patrol - Star Hunter
+    Estimated Time: ~6 or 7 hours?
+    Mods Chosen:
+        - Track a high score that persists across scenes and display it in the UI (5)
+        - Implement the 'FIRE' UI text from the original game (5)
+        - Create a new scrolling tile sprite for the background (5)
+        - Allow the player to control the Rocket after it's fired (5)
+
+        - Display the time remaining (in seconds) on the screen (10)
+        - Implement parallax scrolling for the background (10)
+
+        - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15)
+        - Implement an alternating two-player mode (15)
+        - Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (15)
+
+    TA Approved Mods:
+        - Randomize each spaceship's vertical position at the start of each play (5)
+        - When rocket is fired, allow player's contol to tilt the rocket. Also sets the rocket's horizontal speed by severity of rocket angle (15)
+
+    All sources used:
+        - Base Rocket Patrol
+        - Nathan's asset pack for Rocket Patrol
+        - Phaser Docs
+
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
